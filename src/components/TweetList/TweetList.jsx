842c3576b119bae7647e652d9a91d0b20c2dd344
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-05-18 09:01:01 +0300

    add styles

--- src/components/TweetList/TweetList.jsx
+++ src/components/TweetList/TweetList.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { TweetCard } from '../TweetCard/TweetCard';
+import css from './TweetList.module.css';
 
 export const TweetList = ({ users, onClick }) => (
-  <div>
-    <ul>
+  <div className={css.wrapper}>
+    <ul className={css.list}>
       {users.map(user => (
-        <TweetCard
-          key={user.id}
-          id={user.id}
-          avatar={user.avatar}
-          followers={user.followers.toLocaleString('en-US')}
-          tweets={user.tweets.toLocaleString('en-US')}
-          isFollowing={user.isFollowing}
-          onClick={onClick}
-        ></TweetCard>
+        <li key={user.id}>
+          <TweetCard
+            id={user.id}
+            avatar={user.avatar}
+            followers={user.followers.toLocaleString('en-US')}
+            tweets={user.tweets.toLocaleString('en-US')}
+            isFollowing={user.isFollowing}
+            onClick={onClick}
+          ></TweetCard>
+        </li>
       ))}
     </ul>
-    <TweetCard></TweetCard>
   </div>
 );
