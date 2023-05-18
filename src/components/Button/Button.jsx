3f62acc3ec457cb04f976ff8539ba913bfa4e222
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-05-18 08:29:58 +0300

    add button styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
+import css from './Button.module.css';
 
 export const Button = ({ onClick, id, isFollowing }) => (
-  <button type="button" onClick={onClick} id={id}>
-    {isFollowing ? 'Following' : 'Follow'}
-  </button>
+  <div className={css.wrapper}>
+    <button
+      type="button"
+      onClick={onClick}
+      id={id}
+      className={classNames(css.button, isFollowing ? css.following : css.follow)}
+    >
+      {isFollowing ? 'Following' : 'Follow'}
+    </button>
+  </div>
 );
